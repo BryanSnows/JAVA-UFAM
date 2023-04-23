d6efd5bb1b1e56cc15ac2d3f414305e80486a1d6
bryan neves pinto <bryan.pinto@conecthus.org.br>
2023-04-23 17:52:38 -0400

    feat: add

---
+++ ufam_aulas/helo_word/Aluno.java
@@ -0,0 +1,19 @@
+public class Aluno {
+
+
+    private String nome;
+
+    public String getNome(){
+        return this.nome;
+    
+    }
+
+    public void setNome(String novoNome){
+        this.nome = novoNome;
+    
+    }
+
+    public void digaSeuNome(){
+        System.out.println(this.getNome());
+    }
+}
---
+++ ufam_aulas/helo_word/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String args[]){
+        Aluno marco, atos, amigo;
+        marco = new Aluno();
+        atos = new Aluno();
+        amigo =  new Aluno();
+
+        marco.setNome("marco");
+        marco.digaSeuNome();
+    }
+}

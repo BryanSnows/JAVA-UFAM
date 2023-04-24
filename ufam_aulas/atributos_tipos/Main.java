59357a533fe4db262de0c79e146e83587c26ae6a
bryan neves pinto <bryan.pinto@conecthus.org.br>
2023-04-23 21:30:09 -0400

    feat: add

---
+++ ufam_aulas/atributos_tipos/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void  main(String args []){
+        int contador, cont1, cont2;
+
+        contador = 4;
+        cont1 = 3;
+        cont2 = 4;
+
+        System.out.println(cont1+cont2);
+        System.out.println(contador);
+    }
+    
+
+
+
+}
